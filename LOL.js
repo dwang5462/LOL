69e8d1bfa5188397fdc887588d4a5c51525e9090
Kevin Costello <kevincostello213@gmail.com>
2016-01-05 20:29:36 -0800

    message

--- LOL.js
+++ LOL.js
@@ -21,9 +21,24 @@ var idToChamp = function(data) {
          url: "https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion/21?api_key=" + apiKey,
 	      success: function(data){        
 	         console.log(data);
+	         lastTenGames(data);
          },
          failure: function(data){
             console.log("Failure");
          }
    });
+}
+
+var lastTenGames = function(data) {
+	$.ajax({ 
+         type: "GET",
+         url: "https://na.api.pvp.net/api/lol/na/v2.2/matchlist/by-summoner/" +
+         "34901603?seasons=SEASON2015&beginIndex=0&endIndex=10&api_key=fb83f974-bb7e-4fb5-a0a1-5b0036b8e57d",
+	      success: function(data){        
+	         console.log(data);
+         },
+         failure: function(data){
+            console.log("Failure");
+         }
+      });
 }
